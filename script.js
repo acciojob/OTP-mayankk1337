bfd7a1431524963f9653b01cdb365cd805586592
mayankk1337 <ritiksingh2428@gmail.com>
2025-09-30 06:25:54 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll('.code')  ;
+
+inputs.forEach( (input, index) => {
+
+    input.addEventListener('input', () => {
+        if( input.value.match(/[0-9]/) ){
+
+            if( index < inputs.length - 1 ){
+                inputs[index + 1].focus() ;
+            }
+
+        } else {
+            input.value = '' ;
+        }
+
+    } ) ;
+
+    input.addEventListener('keydown', (e) => {
+             if( e.key === 'Backspace' ){
+
+            if( !input.value && index > 0 ){
+                inputs[index - 1].focus() ;
+            }
+
+        }
+
+    } ) ;
+
+}) ;
